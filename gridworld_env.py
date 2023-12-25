73019639bacd541726863c031d1a41d10b3851ab
Benteng Ma <benteng.ma@kcl.ac.uk>
2023-12-25 19:18:49 +0000

    Added comments.

--- gridworld_env.py
+++ gridworld_env.py
@@ -2,19 +2,17 @@ import gym
 from gym import spaces
 import numpy as np
 import random
-import matplotlib.pyplot as plt
-import matplotlib.colors as mcolors
 import pygame
 from enum import Enum
 
-
+# Define action enums for clarity in action representation
 class Action(Enum):
     UP = 0
     DOWN = 1
     LEFT = 2
     RIGHT = 3
 
-
+# Define cell types to standardize the cell representation in the grid
 class CellType(Enum):
     TARGET = 'T'
     START = 'S'
@@ -23,14 +21,14 @@ class CellType(Enum):
     TRAP = 'X'
     EMPTY = '.'
 
-
+# Custom environment class inheriting from gym.Env
 class GridWorldEnv(gym.Env):
     metadata = {'render.modes': ['human']}
 
     def __init__(self, layout_file, success_prob=0.8, rewards_map=None):
         super(GridWorldEnv, self).__init__()
 
-        # Default rewards map
+        # Default rewards map, can be overridden by external rewards_map
         default_rewards_map = {
             CellType.TARGET: 0.0,
             CellType.START: -1.0,
@@ -40,55 +38,65 @@ class GridWorldEnv(gym.Env):
             CellType.EMPTY: -1.0
         }
 
+        # Use provided rewards map or fall back to default
         self.rewards_map = rewards_map if rewards_map else default_rewards_map
+        # Load the grid layout from a file
         self.layout = self._load_layout(layout_file)
         self.grid_size_x, self.grid_size_y = self.layout.shape
 
+        # Define the action and observation spaces
         self.action_space = spaces.Discrete(len(Action))
         self.observation_space = spaces.Box(low=0, high=max(self.grid_size_x, self.grid_size_y), 
                                             shape=(2,), dtype=np.int32)
 
+        # Initialize state and success probability
         self.state = None
         self.success_prob = success_prob
 
-        # Pygame setup
+        # Pygame setup for rendering
         self.window_size = (self.grid_size_y * 50, self.grid_size_x * 50)
         pygame.init()
         self.screen = pygame.display.set_mode(self.window_size)
         self.clock = pygame.time.Clock()
 
     def _load_layout(self, filename):
-        # [Your existing _load_layout method, modified to use CellType enums]
+        # Load layout from a file and convert each cell to a CellType enum
         with open(filename, 'r') as f:
             layout = [list(line.strip()) for line in f.readlines()]
         return np.array(layout, dtype=CellType)
-    
+
     def get_reward(self, state):
+        # Get the reward for a given state based on the current layout
         cell_type_str = self.layout[state[0], state[1]]
         cell_type_enum = CellType(cell_type_str)
         return self.rewards_map[cell_type_enum]
 
     def get_type(self, state):
+        # Get the type of a cell in a given state
         return self.layout[state[0], state[1]]
 
     def find_positions(self, cell_type):
+        # Find all positions of a given cell type in the grid
         return [(i, j) for i in range(self.grid_size_x) for j in range(self.grid_size_y) if self.layout[i, j] == cell_type]
 
     def get_terminal_states(self):
+        # Get all states that are considered terminal (end of episode)
         return self.find_positions(CellType.TARGET.value)
 
     def reset(self):
+        # Reset the environment to the initial state
         start_positions = self.find_positions(CellType.START.value)
         if not start_positions:
             raise ValueError("No starting position in layout")
-        self.state = start_positions[0]  # Choose the first start position
+        self.state = start_positions[0]
         return np.array(self.state)
 
     def step(self, action):
+        # Perform an action in the environment and return the result
         action_enum = Action(action)
         assert self.action_space.contains(action), f"{action_enum} is an invalid action"
 
-        # Determine if the action is successful or slips
+        # Determine the result of the action with a probability of slipping
         if random.random() < self.success_prob:
             move = self._action_to_move(action_enum)
         else:
@@ -96,7 +104,7 @@ class GridWorldEnv(gym.Env):
 
         new_state = (self.state[0] + move[0], self.state[1] + move[1])
 
-        # Check boundaries and obstacles
+        # Check for boundaries and obstacles, update state if valid
         if 0 <= new_state[0] < self.grid_size_x and 0 <= new_state[1] < self.grid_size_y and \
         self.layout[new_state[0], new_state[1]] != CellType.OBSTACLE.value:
             self.state = new_state
@@ -107,6 +115,7 @@ class GridWorldEnv(gym.Env):
         return np.array(self.state), reward, done, {}
 
     def _action_to_move(self, action_enum):
+        # Map an action enum to a movement (delta x, delta y)
         action_mapping = {
             Action.UP: (-1, 0),
             Action.DOWN: (1, 0),
@@ -116,12 +125,14 @@ class GridWorldEnv(gym.Env):
         return action_mapping[action_enum]
 
     def _random_side_move(self, action_enum):
+        # Determine a random side move for the slipping action
         if action_enum in [Action.UP, Action.DOWN]:
             return random.choice([(-1, 0), (1, 0)])
         else:
             return random.choice([(0, -1), (0, 1)])
 
     def render(self, mode='human'):
+        # Render the environment's current state
         if mode == 'human':
             colors = {
                 CellType.EMPTY.value: (192, 192, 192),
@@ -132,28 +143,31 @@ class GridWorldEnv(gym.Env):
                 CellType.TRAP.value: (255, 0, 0)
             }
 
+            # Draw each cell in the grid
             for i in range(self.grid_size_x):
                 for j in range(self.grid_size_y):
                     color = colors[self.layout[i, j]]
                     rect = pygame.Rect(j * 50, i * 50, 50, 50)
                     pygame.draw.rect(self.screen, color, rect)
 
+            # Draw the agent's current position
             agent_color = (255, 255, 255)  # White
             agent_rect = pygame.Rect(self.state[1] * 50, self.state[0] * 50, 50, 50)
             pygame.draw.rect(self.screen, agent_color, agent_rect)
 
-            pygame.display.flip()
-            self.clock.tick(60)
+            pygame.display.flip()  # Update the display
+            self.clock.tick(60)    # Control the frame rate
 
-
+# Main function to run the environment
 if __name__ == '__main__':
     env = GridWorldEnv('layout.txt')
     state = env.reset()
     done = False
 
+    # Run the environment until a terminal state is reached
     while not done:
-        action = env.action_space.sample()
+        action = env.action_space.sample()  # Sample a random action
         state, reward, done, _ = env.step(action)
-        env.render()
+        env.render()  # Render the current state
 
-    pygame.quit()
+    pygame.quit()  # Clean up Pygame
